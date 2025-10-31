d9f12b3b398b1d3b6ca3b2650cee24ba5d17edb8
al-stasevich <a.stasevich2021@gmail.com>
2025-10-31 13:58:04 +0300

    feat: added design system - defined color palette and theme, created text variants

---
+++ src/theme/palette.ts
@@ -0,0 +1,16 @@
+export const palette = {
+  primary: "#5EDE99",
+  secondary: "#F288F4",
+  success: "#00CB6A",
+  warning: "#EFBE24",
+  danger: "#F26666",
+
+  dark: "#04150C",
+  light: "#F7F7F7",
+
+  black: "#152C07",
+  gray: "#5D5C5D",
+  gray2: "#BEBEBE",
+
+  white: "#FFFFFF",
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,117 @@
+import { FontWeight } from "./../../node_modules/react-native-svg/lib/typescript/lib/extract/types.d";
+import { createTheme } from "@shopify/restyle";
+import { palette } from "./palette";
+
+export const theme = createTheme({
+  spacing: {
+    xs: 4,
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 32,
+    "2xl": 48,
+  },
+
+  colors: {
+    primary: palette.primary,
+    secondary: palette.secondary,
+    success: palette.success,
+    warning: palette.warning,
+    danger: palette.danger,
+    dark: palette.dark,
+
+    mainBackground: palette.light,
+    cardBackground: palette.white,
+
+    textPrimary: palette.black,
+    textSecondary: palette.gray,
+    textTertiary: palette.gray2,
+    textFourtiary: palette.primary,
+    textInverted: palette.white,
+
+    white: palette.white,
+
+    iconColor: palette.gray2,
+    iconActiveColor: palette.primary,
+  },
+
+  textVariants: {
+    headerLX: {
+      fontSize: 32,
+      fontWeight: "700",
+      color: "textPrimary",
+    },
+    headerLarge: {
+      fontSize: 16,
+      fontWeight: "700",
+      color: "textPrimary",
+    },
+    subtitle: {
+      fontSize: 14,
+      fontWeight: "600",
+      color: "textFourtiary",
+    },
+    bodyXL: {
+      fontSize: 20,
+      fontWeight: "700",
+      color: "textPrimary",
+    },
+    bodyLarge: {
+      fontsize: "16",
+      fontWeight: "700",
+      color: "textPrimary",
+    },
+    bodyLarge2: {
+      fontsize: "16",
+      fontWeight: "600",
+      color: "textPrimary",
+    },
+    bodyLarge3: {
+      fontsize: "16",
+      fontWeight: "400",
+      color: "textSecondary",
+      lineHeight: "140%",
+    },
+    bodyMiddle: {
+      fontsize: "14",
+      fontWeight: "400",
+      color: "textPimary",
+    },
+    bodyMiddle2: {
+      fontsize: "14",
+      fontWeight: "400",
+      color: "textSecondary",
+    },
+    bodyMiddle3: {
+      fontsize: "14",
+      fontWeight: "400",
+      color: "textTertiary",
+    },
+    bodySmall: {
+      fontsize: "10",
+      fontWeight: "400",
+      color: "textSecondary",
+    },
+    bodySmall2: {
+      fontsize: "10",
+      fontWeight: "400",
+      color: "textTertiary",
+    },
+
+    buttonLarge: {
+      fontsize: "32",
+      fontWeight: "700",
+      color: "textInverted",
+    },
+    buttonMiddle: {
+      fontsize: "16",
+      fontWeight: "600",
+      color: "textInverted",
+    },
+    buttonSmall: {
+      fontsize: "12",
+      fontWeight: "600",
+      color: "textInverted",
+    },
+  },
+});
